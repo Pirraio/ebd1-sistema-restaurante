81e946bea6e5ce005c93ab46903a8acc6fdd87bd
jose cassio <josecassio2013@gmail.com>
2024-08-17 21:27:10 -0300

    refactor: remodeling business rules

--- main.c
+++ main.c
@@ -6,74 +6,54 @@ typedef struct dish {
     char name[50];
 } Dish;
 
+
 typedef struct {
     Dish *dishes;
     int count;
-} Menu;
+    int isProcessing;
+} Order;
 
-void addDish(Menu *menu, int *counter, const char *name) {
-    if (*counter == 0) {
-        menu->dishes = (Dish *)malloc(sizeof(Dish));
-    } else {
-        menu->dishes = (Dish *)realloc(menu->dishes, (*counter + 1) * sizeof(Dish));
-    }
-    menu->dishes[*counter].id = *counter + 1;
-    snprintf(menu->dishes[*counter].name, sizeof(menu->dishes[*counter].name), "%s", name);
-    (*counter)++;
-    menu->count = *counter; 
-}
-
-void listMenu(Menu *menu) {
-    for (int i = 0; i < menu->count; i++) {
-        printf("Prato %d: %s\n", menu->dishes[i].id, menu->dishes[i].name);
-    }
-}
-
-void removeDish(Menu *menu, int *counter, int id) {
-    int found = 0;
-    for (int i = 0; i < *counter; i++) {
-        if (menu->dishes[i].id == id) {
-            found = 1;
-        }
-        if (found && i < *counter - 1) {
-            menu->dishes[i] = menu->dishes[i + 1];
-        }
-    }
-    if (found) {
-        (*counter)--;
-        menu->dishes = (Dish *)realloc(menu->dishes, (*counter) * sizeof(Dish));
-        menu->count = *counter; 
-    }
-}
 
 int main() {
-    Menu entries = {NULL, 0};
-    Menu mains = {NULL, 0};
-    Menu desserts = {NULL, 0};
 
-    int entriesCounter = 0;
-    int mainsCounter = 0;
-    int dessertsCounter = 0;
+    Dish entries[5] = {
+        {1, "Sopa de Cebola"},
+        {2, "Salada Caesar"},
+        {3, "Bruschetta"},
+        {4, "Carpaccio de Carne"},
+        {5, "Camarão ao Alho"}
+    };
+    Dish mains[5] = {
+        {1, "Lasanha à Bolonhesa"},
+        {2, "Filé Mignon com Fritas"},
+        {3, "Frango Grelhado com Legumes"},
+        {4, "Bacalhau à Gomes de Sá"},
+        {5, "Risoto de Cogumelos"}
+    };
+    Dish desserts[5] = {
+        {1, "Tiramisu"},
+        {2, "Cheesecake de Frutas Vermelhas"},
+        {4, "Mousse de Chocolate"},
+        {3, "Pudim de Leite"},
+        {5, "Sorvete de Baunilha com Calda de Morango"}
+    };
 
-    addDish(&entries, &entriesCounter, "Sopa de Cebola");
-    addDish(&entries, &entriesCounter, "Salada Caesar");
-    addDish(&mains, &mainsCounter, "Lasanha à Bolonhesa");
-    addDish(&mains, &mainsCounter, "Filé Mignon com Fritas");
-    addDish(&desserts, &dessertsCounter, "Tiramisu");
-    addDish(&desserts, &dessertsCounter, "Cheesecake de Frutas Vermelhas");
+    printf("Bem-vindo ao Restaurante!\n");
+    printf("Opções:\n");
+    printf("1 - Criar Pedido\n");
+    printf("2 - Remover Pedido\n");
+    printf("3 - Listar pedidos pendentes\n");
+    printf("4 - Listar pedidos em processamento\n");
+    printf("5 - Processar pedido\n");
+    printf("6 - Sair\n");
 
-    printf("Entradas:\n");
-    listMenu(&entries);
+    int option;
+    scanf("%d", &option);
 
-    printf("\nRemovendo 'Salada Caesar'...\n");
-    removeDish(&entries, &entriesCounter, 2);
 
-    printf("Entradas após remoção:\n");
-    listMenu(&entries);
 
-    free(entries.dishes);
-    free(mains.dishes);
-    free(desserts.dishes);
+
+   
 
     return 0;
 }
